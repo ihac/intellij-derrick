71f2e9ade4a307777d030ede469241a6e8f75725
Xiao An <hac@zju.edu.cn>
2018-06-05 22:07:26 +0800

    add derrick support
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- src/xyz/ihac/intellij/plugin/derrick/addon/Derrick.scala
+++ src/xyz/ihac/intellij/plugin/derrick/addon/Derrick.scala
@@ -4,40 +4,39 @@ import java.nio.file.Paths
 
 import com.intellij.execution.ExecutionException
 import play.api.libs.json.{JsArray, JsObject, JsValue, Json}
+import xyz.ihac.intellij.plugin.derrick.logging.Logger
 import xyz.ihac.intellij.plugin.derrick.util.Command
+
 import collection.JavaConverters._
 
-class Derrick(val workDir: String) {
+class Derrick(val path: String, val workDir: String) {
   private def call(subcomm: String, param: List[String]): JsValue = {
     val newParams = subcomm::param:::List("--output", "json", "--silent")
-    val comm =
-        new Command(Paths.get(workDir), "derrick_mock", newParams:_*)
-    try {
-      comm.exec()
-    } catch {
-      case e: ExecutionException => throw e
-    }
+    val comm = new Command(Paths.get(workDir), path, newParams:_*)
+
+    comm.exec()
+  }
+
+  def version(): JsValue = {
+    call("--version", Nil)
   }
 
   def init(rigging: String, params: Map[String, String]): JsValue = {
-    try {
-      if (rigging != null && params != null) {
-        val params_str = "%s".format(Json.toJson(params).toString)
-        call("init", List("--rigging", rigging, "--params", params_str))
-      }
-      else
-        call("init", Nil)
+    if (rigging != null && params != null) {
+      val params_str = "%s".format(Json.toJson(params).toString)
+      call("init", List("--rigging", rigging, "--params", params_str))
     }
-    catch {
-      case e: Exception => throw e
-    }
+    else
+      call("init", Nil)
   }
 
 }
+
 object Derrick {
-  def get_riggings_and_params(workDir: String): java.util.Map[String, java.util.List[java.util.Map[String, String]]] = {
-    val derrick = new Derrick(workDir)
+  def get_riggings_and_params(comm: String, workDir: String): java.util.Map[String, java.util.List[java.util.Map[String, String]]] = {
+    val derrick = new Derrick(comm, workDir)
     val res = derrick.init(null, null)
+
     val status = (res \ "status").validate[String].get
     val code = (res \ "code").validate[String].get
 
@@ -54,5 +53,16 @@ object Derrick {
     }
     else
       throw new ExecutionException(res("code").as[String] + ": " + res("message").as[String])
+  }
+
+  def verify(comm: String, workDir: String): Boolean = {
+    val derrick = new Derrick(comm, workDir)
+    try {
+      val res = derrick.version()
+      (res \ "version").validate[String].isSuccess
+    }
+    catch {
+      case e => false
+    }
   }
 }
