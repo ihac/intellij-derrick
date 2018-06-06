379a4c504b3a1b820c993475d597d5bf2d5ccc8f
Xiao An <hac@zju.edu.cn>
2018-06-06 14:36:19 +0800

    add common types
    Signed-off-by: Xiao An <hac@zju.edu.cn>

---
+++ src/xyz/ihac/intellij/plugin/derrick/common/Config.scala
@@ -0,0 +1,41 @@
+package xyz.ihac.intellij.plugin.derrick.common
+
+
+class Config(c: MyObject) {
+  lazy val config: Map[String, MyValue] = c match {
+    case MyObject(fields) => fields
+    case _ => throw new RuntimeException("error in parsing config: " + c.toString)
+  }
+
+  def apply(key: String): MyValue = config(key)
+}
+
+sealed class MyValue
+
+case class MyBoolean(value: Boolean) extends MyValue {
+  override def toString: String = value.toString
+  def toBoolean: Boolean = value
+}
+
+case class MyString(value: String) extends MyValue {
+  override def toString: String = value
+}
+
+case class MyInt(value: Int) extends MyValue {
+  override def toString: String = value.toString
+  def toInt: Int = value
+}
+
+case class MyList(l: List[MyValue]) extends MyValue {
+  override def toString: String = "[%s]".format(l mkString ", ")
+}
+
+case class MyObject(obj: Map[String, MyValue]) extends MyValue {
+  def toScalaMap: Map[String, MyValue] = obj
+  override def toString: String = "{\n%s\n}".format(
+    obj map {
+      case (key, value) => "\t" + key + ": " + value
+    } mkString ",\n"
+  )
+}
+
---
+++ src/xyz/ihac/intellij/plugin/derrick/common/Image.scala
@@ -0,0 +1,9 @@
+package xyz.ihac.intellij.plugin.derrick.common
+
+class Image(val name: String, val tag: String, val uuid: String) {
+
+  def this(name: String) = this(name, "latest", "")
+
+  def getId: String = uuid
+  override def toString: String = "%s:%s".format(name, tag)
+}
